7af18e00c049334f48a8c521773786467123e96b
clpowe <clpowe@gmail.com>
2022-03-14 18:13:03 -0400

    hosting

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,14 +1,17 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
-  rules: {
-    quotes: ["error", "double"],
-  },
-};
+	root: true,
+	env: {
+		es6: true,
+		node: true
+	},
+	extends: ['eslint:recommended', 'google'],
+	rules: {
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'quote-props': ['error', 'as-needed'],
+		'no-tabs': ['error', { allowIndentationTabs: true }],
+		'object-curly-spacing': ['error', 'always'],
+		indent: ['error', 'tab'],
+		'comma-dangle': ['error', 'never']
+	}
+}
